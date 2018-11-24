c2a19c900563ab4c8b83f2f2b5e8caa4b44ad6aa
Vova <vettel070897@gmail.com>
2018-11-24 23:36:31 +0200

    add gitignore

--- src/MainPackage/HomeWork/week2/_11.java
+++ src/MainPackage/HomeWork/week2/_11.java
@@ -1,8 +1,12 @@
 package MainPackage.HomeWork.week2;
 
+import MainPackage.HomeWork.Function.ForWorkWithArray;
+
 public class _11 {
     public static void main(String[] args) {
         // test
+        int[] mas = ForWorkWithArray.createArray(10);
+        ForWorkWithArray.printArray(mas);
         System.out.println(" ");
     }
 }
